3585decbd987081dbb019f7c3ebd9fd47dded5c8
Ashfaque <ashfaquemuhammed94@gmail.com>
2026-01-29 19:24:06 +0530

    Bug fixed

--- CLIENT/src/configs/api.ts
+++ CLIENT/src/configs/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: 'https://autothumb-ai-platform.vercel.app', // Backend server URL
+  baseURL: "https://autothumb-server.vercel.app", // Backend server URL
   withCredentials: true, // Include cookies in requests
 });
 export default api;
