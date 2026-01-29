d2f5648732bd3f13381a784cc7279fc8aa89e020
Ashfaque <ashfaquemuhammed94@gmail.com>
2026-01-29 20:27:46 +0530

    Shift to render

--- CLIENT/src/configs/api.ts
+++ CLIENT/src/configs/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: "https://autothumb-server.vercel.app", // Backend server URL
+  baseURL: "https://autothumb-ai-backend.onrender.com", // Backend server URL
   withCredentials: true, // Include cookies in requests
 });
 export default api;
